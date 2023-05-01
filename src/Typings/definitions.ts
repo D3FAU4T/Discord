854050118766c963a076ede1eca7390c92b46dd0
D3FAU4T <89858397+D3FAU4T@users.noreply.github.com>
2023-05-01 01:16:03 +0000

    Added types for Merriam-Webster API

--- src/Typings/definitions.ts
+++ src/Typings/definitions.ts
@@ -30,3 +30,47 @@ export interface dictionaryAPI {
     },
     sourceUrls: string[];
 }
+
+export interface MerriamWebsterAPI {
+    meta: {
+        id: string;
+        uuid: string;
+        src: string;
+        section: string;
+        stems: string[];
+        offensive: boolean;
+    };
+    hwi: {
+        hw: string;
+        prs: Prs[];
+    };
+    fl: string;
+    def: Definition[];
+    shortdef: string[];
+}
+
+interface Prs {
+    mw: string;
+    sound: {
+        audio: string;
+    };
+}
+
+interface Definition {
+    sseq: Sseq[][][];
+}
+
+interface Sseq {
+    dt: Dt[];
+    sdsense?: Sdsense;
+}
+
+interface Dt {
+    0: "text";
+    1: string;
+}
+
+interface Sdsense {
+    sd: string;
+    dt: Dt[];
+}
