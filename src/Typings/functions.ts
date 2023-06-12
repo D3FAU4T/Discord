5b75c961269a821d2c88b60c592138e03b5ea731
D3FAU4T <d3fau4t@gmail.com>
2023-06-12 17:17:56 +0530

    Added typings for getTwitchData function

--- src/Typings/functions.ts
+++ src/Typings/functions.ts
@@ -1,2 +1,18 @@
 export type parseOpts = 'json' | 'text' | 'html' | 'js' | 'python' | 'diff';
 export type axiosMethods = "get" | "post" | "patch" | "delete";
+
+export interface twitchDataSuccessResponse {
+    id: string,
+    displayName: string,
+    createdAt: string,
+    updatedAt: string,
+    deletedAt: string,
+    userType: "" | "staff" | "admin" | "global_mod",
+    broadcasterType: "partner" | "affiliate" | "",
+    unavailableReason: string
+}
+
+export interface twitchDataFailureResponse {
+    status: number,
+    error: string
+}
