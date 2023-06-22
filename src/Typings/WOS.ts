da9d73baba3ed349e807d46d576bafb7bf0df129
D3FAU4T <89858397+D3FAU4T@users.noreply.github.com>
2023-06-22 09:29:44 +0000

    Added typings for level data structure

--- src/Typings/WOS.ts
+++ src/Typings/WOS.ts
@@ -8,3 +8,18 @@ export interface WOSCorrectResponse {
 export interface WOSErrorResponse {
     error: "Level not found" | "An error occurred";
 }
+
+export interface WOSLevel {
+    Level: string;
+    Language: string;
+    column1: WOScolumn[];
+    column2: WOScolumn[];
+    column3: WOScolumn[];
+}
+
+interface WOScolumn {
+    word: string;
+    username: string;
+    locked: boolean;
+    index: number;
+}
