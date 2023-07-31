611e9b170141f9eec471313903dae3bb5bb6f6d7
D3FAU4T <89858397+D3FAU4T@users.noreply.github.com>
2023-07-31 22:59:22 +0000

    Added monitor for Frogziiia instruction channel

--- src/Events/messageReactionRemove.ts
+++ src/Events/messageReactionRemove.ts
@@ -5,6 +5,12 @@ export default new Event('messageReactionRemove', async (reaction, user) => {
     if (reaction.partial) await reaction.fetch();
     if (user.bot || !reaction.message.guild) return;
     if (reaction.message.channel.id == '1050395057687101440') {
-        if (reaction.emoji.name === '🎨') await reaction.message.guild.members.cache.get(user.id)?.roles.remove('1050443777627660358');
+        if (reaction.emoji.name === '🎨')
+          await reaction.message.guild.members.cache.get(user.id)?.roles.remove('1050443777627660358');
     }
+
+  else if (reaction.message.channel.id == '1133305588101693490') {
+    if (reaction.emoji.name === '🎨')
+      await reaction.message.guild.members.cache.get(user.id)?.roles.remove('1134059736459903096');
+  }
 });
