ff951a8ed7084c0300598b2f9d9f261e456faa00
D3FAU4T <d3fau4t@gmail.com>
2025-07-11 12:34:38 +0530

    Disabled some commands temporarily

--- src/Music/disconnect.ts
+++
@@ -1,9 +0,0 @@
-import { MusicEvent } from "../Typings/event";
-import type { MusicMetadata } from "../Typings/music";
-
-export default new MusicEvent('disconnect', queue => {
-    let metadata = queue.metadata as MusicMetadata;
-    let channelId = metadata.interaction.channel?.id;
-    if (channelId !== undefined)
-        metadata.client.RadioChannels = metadata.client.RadioChannels.filter(channel => channel.id !== channelId);
-});
